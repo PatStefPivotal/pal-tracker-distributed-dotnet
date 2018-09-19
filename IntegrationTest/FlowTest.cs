552f5d8ba59a39b11366999f9a94145eb23cc0c5
Patrick St-Amant <patrick.stamant@desjardins.com>
2018-09-19 15:17:53 -0400

    Eureka

--- IntegrationTest/FlowTest.cs
+++ IntegrationTest/FlowTest.cs
@@ -25,6 +25,7 @@ namespace IntegrationTest
                 .AppName("RegistrationServer")
                 .Port(8883)
                 .Database("tracker_registration_dotnet_test")
+                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDREGISTERWITHEUREKA", "false")
                 .Build();
 
             _allocationsServer = TestAppServerBuilder()
@@ -32,6 +33,7 @@ namespace IntegrationTest
                 .Port(8881)
                 .Database("tracker_allocations_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
+                .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
 
             _backlogServer = TestAppServerBuilder()
@@ -39,6 +41,7 @@ namespace IntegrationTest
                 .Port(8882)
                 .Database("tracker_backlog_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
+                .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
 
             _timesheetsServer = TestAppServerBuilder()
@@ -46,6 +49,7 @@ namespace IntegrationTest
                 .Port(8884)
                 .Database("tracker_timesheets_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
+                    .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
         }
 
@@ -90,7 +94,7 @@ namespace IntegrationTest
             response = _httpClient.Get(_allocationsServer.Url());
             Assert.Equal("Noop!", response.Content.AsString());
 
-            var createdAllocationId = _httpClient.Post( _allocationsServer.Url($"/allocations?projectId={createdProjectId}"), new Dictionary<string, object>
+            var createdAllocationId = _httpClient.Post(_allocationsServer.Url($"/allocations?projectId={createdProjectId}"), new Dictionary<string, object>
             {
                 {"projectId", createdProjectId},
                 {"userId", createdUserId},
